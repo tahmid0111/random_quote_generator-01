70ef50faa758ce8d141a9dd521a0018916431bde
nahid <0.1tahmidemam@gmail.com>
2024-10-08 18:21:54 +0600

    added dynamic background-image based on dayOfWeek

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,11 @@
-const apiBaseURL = "https://dummyjson.com/quotes"; // Base API for fetching a specific quote
-const quoteElement = document.getElementById("quote"); // The element where the quote will be displayed
+const apiBaseURL = "https://dummyjson.com/quotes";
+const quoteElement = document.getElementById("quote");
+const quoteElement2 = document.getElementById("author");
 
 // Get today's quote based on the day of the year
 function getQuoteOfTheDay() {
-  // Get the current day of the year (1 to 365)
-  const today = new Date();
-  const start = new Date(today.getFullYear(), 0, 0);
-  const diff = today - start;
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDay) + 1; // Ensure we start from day 1
+  const quoteId = getQuoteId();
 
-  // Map the day of the year to a quote ID (1 to 400)
-  const quoteId = dayOfYear % 400 || 400; // This ensures we cycle between 1 to 400
-
-  // Fetch the quote based on the calculated quoteId
   fetch(`${apiBaseURL}/${quoteId}`)
     .then((response) => response.json())
     .then((data) => {
@@ -23,11 +15,23 @@ function getQuoteOfTheDay() {
       console.error("Error fetching the quote:", error);
     });
 }
+function getQuoteId() {
+  // Get the current day of the year (1 to 365)
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  document.body.style.backgroundImage = `url('/assets/img/${dayOfWeek}.jpg')`;
+  const start = new Date(today.getFullYear(), 0, 0);
+  const diff = today - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const dayOfYear = Math.floor(diff / oneDay) + 1; // Ensure we start from day 1
+  return dayOfYear;
+}
 
 // Function to display the fetched quote
 function displayQuote(quote) {
-  quoteElement.textContent = `"${quote.quote}" - ${quote.author}`;
+  
+  quoteElement.innerHTML = `"${quote.quote}"`;
+  quoteElement2.innerHTML = ` - ${quote.author}`;
 }
 
-// Call the function to get and display today's quote
 getQuoteOfTheDay();
