b6d47fb8ae1d50d8197c874d85ec066ee27f5c6a
nahid <0.1tahmidemam@gmail.com>
2024-10-08 21:27:50 +0600

    added loading state

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,24 +3,31 @@ import { getQuoteId } from "./utils.js";
 const apiBaseURL = "https://dummyjson.com/quotes";
 const quoteElement = document.getElementById("quote");
 const quoteElement2 = document.getElementById("author");
+const loading = document.getElementById("loading");
 
 // Get today's quote based on the day of the year
 function getQuoteOfTheDay() {
   const quoteId = getQuoteId();
 
+  // Display the loading element
+  loading.style.display = "block";
+
   fetch(`${apiBaseURL}/${quoteId}`)
     .then((response) => response.json())
     .then((data) => {
       displayQuote(data);
+      // Hide the loading element
+      loading.style.display = "none";
     })
     .catch((error) => {
       console.error("Error fetching the quote:", error);
     });
 }
+
 // Function to display the fetched quote
-function displayQuote(quote) {
-  quoteElement.innerHTML = `"${quote.quote}"`;
-  quoteElement2.innerHTML = ` - ${quote.author}`;
+function displayQuote(data) {
+  quoteElement.innerHTML = `"${data.quote}"`;
+  quoteElement2.innerHTML = ` - ${data.author}`;
 }
 
 getQuoteOfTheDay();
