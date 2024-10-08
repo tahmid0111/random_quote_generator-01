1459d5c75ce604a223715357aaa6c63f9cdc340c
nahid <0.1tahmidemam@gmail.com>
2024-10-08 20:41:48 +0600

    uploaded images in cloud and divided functions in modular way

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+import { getQuoteId } from "./utils.js";
+
 const apiBaseURL = "https://dummyjson.com/quotes";
 const quoteElement = document.getElementById("quote");
 const quoteElement2 = document.getElementById("author");
@@ -15,21 +17,8 @@ function getQuoteOfTheDay() {
       console.error("Error fetching the quote:", error);
     });
 }
-function getQuoteId() {
-  // Get the current day of the year (1 to 365)
-  const today = new Date();
-  const dayOfWeek = today.getDay();
-  document.body.style.backgroundImage = `url('/assets/img/${dayOfWeek}.jpg')`;
-  const start = new Date(today.getFullYear(), 0, 0);
-  const diff = today - start;
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDay) + 1; // Ensure we start from day 1
-  return dayOfYear;
-}
-
 // Function to display the fetched quote
 function displayQuote(quote) {
-  
   quoteElement.innerHTML = `"${quote.quote}"`;
   quoteElement2.innerHTML = ` - ${quote.author}`;
 }
